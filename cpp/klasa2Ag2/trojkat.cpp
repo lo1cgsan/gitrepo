3f9639d76eaf84a0b3354371492ed29e65b2373e
lo1cgsan <lo1cgsan@github.com>
2017-09-27 12:36:34 +0200

    instrukcja warunkowa kl2ag2

---
+++ cpp/klasa2Ag2/trojkat.cpp
@@ -0,0 +1,23 @@
+/*
+ * trojkat.cpp
+ * Program pobiera trzy boki trojkata,
+ * sprawdza, czy da się z nich zbudować trójkąt,
+ * oblicza obwód i pole (ze wzoru Herona)
+ * i drukuje na ekranie odpowiedni komunikat.
+ */
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    float p;
+    float pole;
+    sqrt(); // pierwiastek
+    return 0;
+}
+
