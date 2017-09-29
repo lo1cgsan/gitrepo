35a98cfd64452f948984d1d980dc902da34541bb
lo1cgsan <lo1cgsan@github.com>
2017-09-29 11:41:52 +0200

    trójkąt i kalkulator

---
+++ cpp/klasa2Ag2/kalkulator.cpp
@@ -0,0 +1,19 @@
+/*
+ * kalkulator.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    char znak; // +, -, *, /
+    
+    if (znak == '+')
+    
+    
+    return 0;
+}
+
--- cpp/klasa2Ag2/trojkat.cpp
+++ cpp/klasa2Ag2/trojkat.cpp
@@ -15,9 +15,21 @@ using namespace std;
 int main(int argc, char **argv)
 {
 
-    float p;
-    float pole;
-    sqrt(); // pierwiastek
+    int a, b, c;
+    int obwod = 0;
+    float p = 0;
+    a = b = c = 0;
+    cout << "Podaj boki: " << endl;
+    cin >> a >> b >> c;
+    if (a + b > c && a + c > b && b + c > a) {
+        cout << "Można zbudować!" << endl;
+        obwod = a + b + c;
+        cout << "Obwód: " << obwod << endl;
+        p = 0.5 * obwod;
+        cout << "p = " << p << endl;
+        cout << "Pole: " << sqrt(p*(p-a)*(p-b)*(p-c)) << endl;
+    }
+    
     return 0;
 }
 
