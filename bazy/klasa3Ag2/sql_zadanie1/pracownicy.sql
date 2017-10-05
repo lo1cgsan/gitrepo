c687246e1a566f4d76c027e28bb7df9bf228141e
lo1cgsan <lo1cgsan@github.com>
2017-10-05 08:45:21 +0200

    bazy_sql kl3ag2

---
+++ bazy/klasa3Ag2/sql_zadanie1/pracownicy.sql
@@ -0,0 +1,23 @@
+CREATE TABLE premia (
+    id VARCHAR(20) PRIMARY KEY,
+    premia NUMERIC
+);
+
+CREATE TABLE dzial (
+    id INTEGER PRIMARY KEY,
+    nazwa VARCHAR(40),
+    siedziba VARCHAR(40)
+);
+
+CREATE TABLE pracownicy (
+    id VARCHAR(6) PRIMARY KEY,
+    nazwisko VARCHAR(20),
+    imie VARCHAR(20),
+    stanowisko VARCHAR(20),
+    data_zatr VARCHAR(23),
+    placa NUMERIC,
+    premia NUMERIC,
+    id_dzial INTEGER,
+    FOREIGN KEY(stanowisko) REFERENCES premia(id),
+    FOREIGN KEY(id_dzial) REFERENCES dzial(id)
+);
