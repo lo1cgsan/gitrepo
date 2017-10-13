b62586054ac488535f74624eecb57f7d9e9f7f32
lo1cgsan <lo1cgsan@gmail.com>
2017-10-13 11:39:43 +0200

    pętle switch kl2ag2

---
+++ cpp/klasa2Ag2/petle_znak.cpp
@@ -0,0 +1,25 @@
+/*
+ * petle_switch.cpp
+ * Program pobiera numer miesiąca i wyświetla jego nazwę
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    char zn='t';
+    
+    
+    while () {
+        cout << "Podaj miesiąc (1-12): ";
+        cin >> m;
+    };
+
+
+    
+    return 0;
+}
+
