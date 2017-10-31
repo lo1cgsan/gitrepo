ac3fb4d693062776a162f4bc030c4c612622b1cc
lo1cgsan <lo1cgsan@gmail.com>
2017-10-31 15:09:28 +0100

    orm dodawanie danych kl3ag1 + python101 kl2ag1g2

--- bazy/klasa3Ag1/orm/pracownicy-orm.py
+++ bazy/klasa3Ag1/orm/pracownicy-orm.py
@@ -4,6 +4,7 @@
 #  pracownicy-orm.py
 
 from peewee import *
+from dane import *  # import funkcji z pliku dane.py
 
 baza_plik = 'pracownicy.sqlite3'
 baza = SqliteDatabase(baza_plik)
@@ -31,8 +32,40 @@ class Pracownik(BazaModel):
     stanowisko = ForeignKeyField(Premia, related_name='pracownicy')
     data_zatr = CharField()
     placa = DecimalField(decimal_places=2)
-    premia = DecimalField(decimal_places=2, default=0)
     id_dzial = ForeignKeyField(Dzial, related_name='pracownicy')
+    premia = DecimalField(decimal_places=2, default=0)
 
 baza.connect()
 baza.create_tables([Premia, Dzial, Pracownik], True)
+
+premia = dane_z_pliku('premia.txt')
+premia = wyczysc_dane(premia, 1)
+
+dzial = dane_z_pliku('dział.txt')
+
+pracownicy = dane_z_pliku('pracownicy.txt')
+pracownicy = wyczysc_dane(pracownicy, 5)
+pracownicy = wstaw_premie(pracownicy, dict(premia))
+
+premia = [dict(zip(Premia._meta.sorted_field_names, row)) for row in premia]
+
+dzial = [dict(zip(Dzial._meta.sorted_field_names, row)) for row in dzial]
+
+pracownicy = [dict(zip(Pracownik._meta.sorted_field_names, row)) for row in pracownicy]
+
+
+
+
+# tworzenie instancji klasy
+#~obiekt = Premia(id = "Sprzątaczka", premia = 0.2)
+#~print(obiekt.id, obiekt.premia)
+#~obiekt.save()
+
+#~obiekt = Premia(id = "Sekretarka", premia = 0.35)
+#~print(obiekt.id, obiekt.premia)
+#~obiekt.save()
+
+
+
+
+
