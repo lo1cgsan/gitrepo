d2271b72fba5d89a0e93e593b12dd0633aa9e39f
lo1cgsan <lo1cgsan@gmail.com>
2017-11-15 09:56:09 +0100

    potega i silnia kl2ag1 podstawy flaska kl3ag1

---
+++ python/kl2ag1/silnia.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  silnia.py
+#  n! = 1 dla n={0, 1}
+#  n! = 1 * ... * n dla N+ - {1}
+
+def silnia_it(n):
+    wynik = ...
+    for i range(..., ...):
+        wynik = ...
+    return wynik
+    
+def main(args):
+"""Funkcja główna"""
+    # pobierz od użytkownika liczbę naturalną
+    # i przypisz ją do zmiennej n
+    # wywołaj funkcję silnia_it() z odpowiednim argumentem
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
