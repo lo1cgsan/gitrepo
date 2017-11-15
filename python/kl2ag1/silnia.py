0086d3ae68efb787b80387a6042dcf3aaa3c4fdf
solus <xinulsw@gmail.com>
2017-11-15 22:39:27 +0100

    Dodanie testów assert do algorytmów potęgi i silnii

--- python/kl2ag1/silnia.py
+++ python/kl2ag1/silnia.py
@@ -5,19 +5,25 @@
 #  n! = 1 dla n={0, 1}
 #  n! = 1 * ... * n dla N+ - {1}
 
+
 def silnia_it(n):
-    wynik = ...
-    for i range(..., ...):
-        wynik = ...
+    wynik = 1
+    for i in range(2, n + 1):
+        wynik = wynik * i
     return wynik
-    
+
+
 def main(args):
-"""Funkcja główna"""
-    # pobierz od użytkownika liczbę naturalną
-    # i przypisz ją do zmiennej n
-    # wywołaj funkcję silnia_it() z odpowiednim argumentem
-    
+    """Funkcja główna"""
+    n = int(input('Podaj liczbę naturalną: '))
+    assert type(n) == int
+    assert silnia_it(0) == 1
+    assert silnia_it(1) == 1
+    assert silnia_it(2) == 2
+    assert silnia_it(3) == 6
+    print('Silnia dla {:d}: {:d}'.format(n, silnia_it(n)))
     return 0
+
 
 if __name__ == '__main__':
     import sys
