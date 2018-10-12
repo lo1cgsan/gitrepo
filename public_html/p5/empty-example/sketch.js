d10106986c1db3841c7b061bfde28bb1e19e50f9
lo1cgsan <lo1cgsan@gmail.com>
2018-10-12 10:12:16 +0200

    przykłady p5js

--- public_html/p5/empty-example/sketch.js
+++ public_html/p5/empty-example/sketch.js
@@ -5,24 +5,24 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  fill('#4caf50');
-  strokeWeight(10);
-  stroke('red');
+  // fill('#4caf50');
+  // strokeWeight(10);
+  // stroke('red');
   ellipse(200, 300, 150, 80);
-  strokeWeight(20);
-  stroke('blue');
-  fill('#006709');
-  ellipse(40, 100, 80, 30);
+  // strokeWeight(20);
+  // stroke('blue');
+  // fill('#006709');
+  // ellipse(40, 100, 80, 30);
 
-  strokeWeight(5);
-  stroke('black');
-  fill('black');
-  line(10, 10, 60, 60);
-  line(60, 10, 10, 60);
+  // strokeWeight(5);
+  // stroke('black');
+  // fill('black');
+  // line(10, 10, 60, 60);
+  // line(60, 10, 10, 60);
 
-  stroke('green');
-  fill('green');
-  point(100,100);
+  // stroke('green');
+  // fill('green');
+  // point(100,100);
 
-  rect(200, 200, 300, 100, 10);
+  // rect(200, 200, 300, 100, 10);
 }
